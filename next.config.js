84c92c5a67dee2c76c2cde7b3e08918b9fbe8adf
lunaloca55 <107638982+lunaloca55@users.noreply.github.com>
2025-08-08 23:01:01 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",  // <=== enables static exports
   reactStrictMode: true,
-  experimental: {
-    appDir: true
-  }
 };
 
 module.exports = nextConfig;
